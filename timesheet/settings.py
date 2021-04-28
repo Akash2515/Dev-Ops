a540c1d183fc96b4e8f2fa6e39cadefacea6d221
Ubuntu <ubuntu@ip-172-31-25-221.ec2.internal>
2021-04-28 12:41:31 +0000

    changes made in settings file for static changes

--- timesheet/settings.py
+++ timesheet/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'timesheet/static')
+    os.path.join(BASE_DIR, 'static')
 ]
 # messaging
 MESSAGE_TAGS = {
